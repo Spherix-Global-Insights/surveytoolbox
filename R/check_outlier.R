ead38801881a9c6a3d50410b8acc8643eb4e6a88
JujuDon <avdonnachie@gmail.com>
2023-01-23 15:08:40 -0500

    Outlier analysis fix

--- R/check_outlier.R
+++ R/check_outlier.R
@@ -3,23 +3,25 @@ check_outlier <- function(dat, var) {
 
   check_var_validity(dat, var, type = "num")
 
-  if(var(na.omit(dat[var])) > 5) { #only include outliers for questions that have a variation of greater than 5
+  if(length(unlist(na.omit(dat[var]))) > 1) { #only check variables that have multiple respondents answering
 
-    up_lim <- quantile(dat[,var], 0.75, na.rm = TRUE)+1.5*IQR(dat[,var], na.rm = TRUE) # iqr outlier criterion
-    low_lim <- quantile(dat[,var], 0.25, na.rm = TRUE)-1.5*IQR(dat[,var], na.rm = TRUE)
+    if(var(na.omit(dat[var])) > 5) { #only include outliers for questions that have a variation of greater than 5
 
-    up_lim <- determine_var(dat, up_lim)[1] # format for checking
-    low_lim <- determine_var(dat, low_lim)[1]
+      up_lim <- quantile(dat[,var], 0.75, na.rm = TRUE)+1.5*IQR(dat[,var], na.rm = TRUE) # iqr outlier criterion
+      low_lim <- quantile(dat[,var], 0.25, na.rm = TRUE)-1.5*IQR(dat[,var], na.rm = TRUE)
 
-    var_metric <- array(1, nrow(dat))
+      up_lim <- determine_var(dat, up_lim)[1] # format for checking
+      low_lim <- determine_var(dat, low_lim)[1]
 
-    bools <- dat[var] > up_lim | dat[var] < low_lim
+      var_metric <- array(1, nrow(dat))
 
-    if(!is.na(match(TRUE, bools))) {
+      bools <- dat[var] > up_lim | dat[var] < low_lim
 
-      return(bools)
+      if(!is.na(match(TRUE, bools))) {
+
+        return(bools)
+      }
     }
-
   }
 }
 
